bae315d6b29252073786e02431e4fd5a58f4dc9a
Koobonik <devkoo@DESKTOP-B7TTNEL.localdomain>
2018-05-03 01:27:11 +0900

    리눅스 등 bash 명령어 내리는 예제

---
+++ connet_server.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main(int args, char **argv)
+{
+    printf("connet linux server\n");
+    //int system(const char *string);
+    int ret = system("sudo apt-get update");
+}
