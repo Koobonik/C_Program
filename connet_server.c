ac90e4a066a8e690ffdf8db05d0a03e1dfc28960
Koobonik <devkoo@Desktop-Koo.localdomain>
2018-05-05 01:53:50 +0900

    이런 명령어도 가능 합니다.

--- connet_server.c
+++ connet_server.c
@@ -4,5 +4,5 @@ void main(int args, char **argv)
 {
     printf("connet linux server\n");
     //int system(const char *string);
-    int ret = system("sudo apt-get update");
+    int ret = system("ls -a");
 }
