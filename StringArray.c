29c29adf07bf502a59c9c97e1f6451cabcbca625
Koobonik <devkoo@Desktop-Koo.localdomain>
2018-05-06 02:40:53 +0900

    문자열 배열

---
+++ StringArray.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+    char * strArr[3] = {"Simple", "String", "Array"};
+    printf("%s \n", strArr[0]);
+    printf("%s \n", strArr[1]);
+    printf("%s \n", strArr[2]);
+}
