8cc18f400df1a3198a9f3dc1d5373b026fb703a1
Koobonik <devkoo@Desktop-Koo.localdomain>
2018-05-08 01:40:54 +0900

    간단한 주석 추가

--- Call_By_Ref_Swap.c
+++ Call_By_Ref_Swap.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+/*  주소값을 받아와서 직접 바꾸어줌 */
 void Swap(int * ptr1, int * ptr2)
 {
     int temp = * ptr1;
@@ -11,6 +12,6 @@ void main()
     int num1 = 10;
     int num2 = 20;
     printf("num1 num2 : %d %d\n", num1, num2);
-    Swap(&num1, &num2);
+    Swap(&num1, &num2);//이 부분에서 서로 값을 바꿈
     printf("num1 num2 : %d %d\n", num1, num2);
 }
