9be24cc4968116c4007a88b6e3c4341c9cb7dbaf
Koobonik <kbi960130@naver.com>
2018-05-10 22:22:00 +0900

    배열 접근

---
+++ Array_Param_Access.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void ShowArrayElem(int * param, int len)
+{
+    int i;
+    for(i=0; i<len; i++)
+    {
+        printf("%d ",param[i]);
+    }
+    printf("\n");
+}
+
+void AddArrayElem(int *param, int len, int add)
+{
+    int i;
+    for(i=0; i<len; i++)
+    {
+        param[i] += add;
+    }
+}
+
+void main()
+{
+    int arr[3] = {1, 2, 3};
+    AddArrayElem(arr, sizeof(arr) / sizeof(int), 1);
+    ShowArrayElem(arr, sizeof(arr) / sizeof(int));
+
+    AddArrayElem(arr, sizeof(arr) / sizeof(int), 2);
+    ShowArrayElem(arr, sizeof(arr) / sizeof(int));
+
+    AddArrayElem(arr, sizeof(arr) / sizeof(int), 3);
+    ShowArrayElem(arr, sizeof(arr) / sizeof(int));
+}
