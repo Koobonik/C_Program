146d7d4b2e0bd6afad00172b1ba53bc1e34241ca
Koobonik <kbi960130@naver.com>
2018-06-17 12:57:10 +0000

    주석 추가

--- Array_Name.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-void main(){
-	int arr[3] = {0, 1, 2};
-	printf("배열의 이름: %p \n", arr);
-	printf("첫 번째 요소: %p \n", &arr[0]);
-	printf("두 번째 요소: %p \n", &arr[1]);
-	printf("세 번째 요소: %p \n", &arr[2]);
-}
