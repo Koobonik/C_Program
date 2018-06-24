301cfc520b5f3638db19a70ab3deaa0b741fe1f9
Koobonik <kbi960130@naver.com>
2018-06-24 12:50:46 +0000

    10진수를 2진수로 출력하는 프로그램

---
+++ Convert_10_to_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main(){
+    int input; // 입력할 숫자
+    int arr[30]; // 배열의 크기
+    int count = 0; // 반복문을 몇번 돌려줄지 정해줄 변수
+
+    printf("정수를 입력하세요 : ");
+    scanf("%d", &input);
+    
+    for(int i = 0; input > 0; i++){
+        arr[i] = input%2; // input 값의 2로 나누었을때의 나머지 값을 넣어줌
+        input = input/2; // 그리고나서 2로 나누어준 값을 다시 대입
+        count++; // 이후 배열을 몇번 반복할지 결정해줄 변수
+    }
+
+    
+    for(int i = count - 1; i >= 0; i--){
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+}
