a3d23e8d3e7487b044a4af7d26dea4af7855b920
Koobonik <kbi960130@naver.com>
2018-06-28 11:04:44 +0900

    주석

--- StringArray.c
+++ StringArray.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 void main(){
-    char * strArr[3] = {"Simple", "String", "Array"};
+    char * strArr[3] = {"Simple", "String", "Array"}; // 0, 1, 2에 각각 문자열 삽입
     printf("%s \n", strArr[0]);
     printf("%s \n", strArr[1]);
     printf("%s \n", strArr[2]);
