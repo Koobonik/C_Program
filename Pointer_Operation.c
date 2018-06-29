a698815693991b5d49b096c8809a861658b7cd8d
Koobonik <kbi960130@naver.com>
2018-06-30 01:40:19 +0900

    주석 추가

--- Pointer_Operation.c
+++ Pointer_Operation.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(void){
 	int num1 = 100, num2 = 100;
-	int * pnum;
+	int * pnum; // pnum 이라는 포인터 변수 선언
 	pnum = &num1;// 포인터 pnum이 num1 을 가리킴
-	(*pnum) += 30;
-	pnum = &num2;
-	(*pnum) -= 50;
+	(*pnum) += 30; // num1 에는 130이 들어간다.
+	pnum = &num2; // pnum이 가리키는 것을 num2로 바꾼다
+	(*pnum) -= 50; // 50이 들어갈 것임
 	printf("num1 : %d , num2 : %d \n", num1, num2);
 	return 0;
 }
