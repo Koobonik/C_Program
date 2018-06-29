e93536193285afdb052bd21d9f7120927b30a58e
Koobonik <kbi960130@naver.com>
2018-06-29 02:40:04 +0000

    포인터 연습에 주석 추가

--- Pointer_Practice.c
+++ Pointer_Practice.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 void main(){
 	int num1 = 10, num2 = 20;
-	int * ptr1 = &num1;
-	int * ptr2 = &num2;
-	(* ptr1) += 10;
-	(* ptr2) -= 10;
-	int * ptr3 = ptr1;//임시 변수
-	ptr1 = ptr2;
-	ptr2 = ptr3;
-	printf("%d %d\n", *ptr1, *ptr2);
+	int * ptr1 = &num1; // 포인터 변수에 num1의 주소값 넣기
+	int * ptr2 = &num2; // 포인터 변수에 num2의 주소값 넣기
+	(* ptr1) += 10; // num1 에 + 10
+	(* ptr2) -= 10; // num2 에 - 10 = 10
+	int * ptr3 = ptr1;//임시 변수 num1 에는 현재 10 + 10 이므로 20이 들어갈 것임
+	ptr1 = ptr2; // 10 이 들어가고
+	ptr2 = ptr3; // 20 이 들어감
+	printf("%d %d\n", *ptr1, *ptr2); // 예상 출력 값은 10 20
 }
