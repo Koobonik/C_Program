f3a2b4c4c46453ca96711d9d8bc334b17b0fcb57
Koobonik <kbi960130@naver.com>
2018-06-30 13:04:31 +0000

    홀수면 앞에서부터 짝수면 뒤에서부터 채워 나가는 프로그램 구현 완료!

--- Odd_Even_Array.c
+++ Odd_Even_Array.c
@@ -6,11 +6,28 @@
 
 #include <stdio.h>
 void main(){
-    int num[10];
-    
+    int num[10]; // 입력 받은 정수 저장 변수
+    int save_num[10]; // 조건에 따라 정수를 저장할 변수
+    int front = 0; // 앞에서부터 채울 때 카운트 할 변수
+    int back = 9; // 뒤에서부터 채울 때 카운트 할 변수
+
     // num에 정수 입력 받기
     for(int i=0; i<10; i++){
         printf("입력: ");
         scanf("%d", &num[i]);
+
+        if(num[i]%2  == 1) // 홀수  인경우
+        {
+            save_num[front] = num[i];
+            front++;
+        }
+        else // 짝수 인경우
+        {
+            save_num[back] = num[i];
+            back--;
+        }
+    }
+    for(int i=0; i<10; i++){
+        printf("%d", save_num[i]);
     }
 }
