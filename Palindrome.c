bbf1e3c35405a706de3009aa852425a0d94690c5
Koobonik <kbi960130@naver.com>
2018-07-01 10:01:51 +0000

    단어 입력 받고 길이 출력하기

--- Palindrome.c
+++ Palindrome.c
@@ -7,11 +7,10 @@
 #include <string.h>
 
 void main(){
-    int str = strlen("hello d"); // 띄어쓰기 포함된다.
-    printf("%d\n", str);
-
-    char c[300];
+    char c[300]; // 단어를  담을 변수
     printf("문자열 입력 : ");
-    scanf("%s", c);
+    scanf("%s", c); // 단어  입력
     printf("입력한 문자열은 %s 입니다\n", c);
+    int char_num = strlen(c); // 단어의 길이 계산
+    printf("%d\n", char_num);
 }
