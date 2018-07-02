2fe63c953c76995d62e853006f731a8c1e25901c
Koobonik <kbi960130@naver.com>
2018-07-02 02:33:13 +0000

    strcmp 함수를 이용하여 쉽게 구현 완료...

--- Palindrome.c
+++ Palindrome.c
@@ -7,26 +7,29 @@
 #include <string.h>
 
 void main(){
-    char c[40]; // 단어를  담을 변수
+    char c[40], d[40]; // 단어를  담을 변수
+    int e = 0; // 문자열 뒤집을때 카운트할때 쓰일 변수
+    int ret; // 문자열을 strcmp 함수를 이용하여 비교할 때 맞으면 0을 반환함
     printf("문자열 입력 : ");
     scanf("%s", c); // 단어  입력
     printf("입력한 문자열은 %s 입니다\n", c);
     int char_num = strlen(c); // 단어의 길이 계산
-    int first_char_num = char_num; // 초기의 길이를 넣음
-    printf("%d\n", char_num);
+    printf("단어의 길이는 %d 입니다.\n", char_num);
 
-    for(int i = 0; i < char_num; i++){
-        if(c[i] == c[char_num - 1]){
-            printf("양 끝이 일치합니다.\n");
-            char_num--;
-            first_char_num--;
-            if(c[i] != c[char_num - 1] ){
-                printf("회문이 아닙니다.\n");
-                break;
-            }
-            else if (char_num - i <= 1){
-                printf("회문 입니다.\n");
-            }
-        }
-    }  
+
+    // 문자열을 반대로 저장할 것임
+    for(int i= char_num -1; i>=0; i--){
+        d[e] = c[i];
+        e++;
+    }
+
+    ret = strcmp(c, d); // 두 단어가 만약 일치한다면 0을 반환할 것임
+
+    if(ret == 0){
+        printf("회문입니다.\n");
+    }
+    else{
+        printf("회문이 아니니다.\n");
+    }
 }
+
---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    char a[20] = "hello";
+    char b[20] = "hello";
+    if(a != b)
+        printf("good");
+
+
+
+}
