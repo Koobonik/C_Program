e907f8857f703f26a1bff1ed545760ea4b985eb2
Koobonik <kbi960130@naver.com>
2018-07-05 01:28:09 +0000

    주석 추가

--- Array_Param.c
+++ Array_Param.c
@@ -4,7 +4,7 @@ void ShowArayElem(int * param, int len)
         int i;
         for(i=0; i<len; i++)
         {
-            printf("%d", param[i]);
+            printf("%d", param[i]); // 배열 요소들을 출력
         }
         printf("\n");
     }
@@ -12,6 +12,6 @@ void main()
 {
     int arr1[3] = { 1, 2, 3 };
     int arr2[5] = { 4, 5, 6, 7, 8};
-    ShowArayElem(arr1, sizeof(arr1) / sizeof(int));
-    ShowArayElem(arr2, sizeof(arr2) / sizeof(int));
+    ShowArayElem(arr1, sizeof(arr1) / sizeof(int)); // arr1 배열을 파라미터로 보내고 길이는 3임
+    ShowArayElem(arr2, sizeof(arr2) / sizeof(int)); // arr2 배열을 파라미터로 보내고 길이는 5임
 }
