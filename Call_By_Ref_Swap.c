c85c5d7e24b534bf10d39bf8a41425629fe7ec15
Koobonik <kbi960130@naver.com>
2018-07-06 06:08:47 +0000

    근본적인 값 자체를 바꾸어 주는 것임

--- Call_By_Ref_Swap.c
+++ Call_By_Ref_Swap.c
@@ -6,12 +6,14 @@ void Swap(int * ptr1, int * ptr2)
     *ptr1 = *ptr2;
     *ptr2 = temp;
 }
-
 void main()
 {
     int num1 = 10;
     int num2 = 20;
     printf("num1 num2 : %d %d\n", num1, num2);
-    Swap(&num1, &num2);//이 부분에서 서로 값을 바꿈
-    printf("num1 num2 : %d %d\n", num1, num2);//값이 서로 바뀜
+    Swap(&num1, &num2);// 이 부분에서 서로 값을 바꿈
+    printf("num1 num2 : %d %d\n", num1, num2);// 값이 서로 바뀜
+
+    Swap(&num1, &num2); // 다시 바꾸어줌
+    printf("num1 num2 : %d %d\n", num1, num2);
 }
