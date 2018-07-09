51f2c50851f9b34c3594014141958ce91b404624
Koobonik <kbi960130@naver.com>
2018-07-09 09:35:32 +0000

    주석 추가

--- Array_Param.c
+++ Array_Param.c
@@ -10,8 +10,8 @@ void ShowArayElem(int * param, int len)
     }
 void main()
 {
-    int arr1[3] = { 1, 2, 3 };
-    int arr2[5] = { 4, 5, 6, 7, 8};
+    int arr1[3] = { 1, 2, 3 }; // 배열 선언
+    int arr2[5] = { 4, 5, 6, 7, 8}; // 배열 선언
     ShowArayElem(arr1, sizeof(arr1) / sizeof(int)); // arr1 배열을 파라미터로 보내고 길이는 3임
     ShowArayElem(arr2, sizeof(arr2) / sizeof(int)); // arr2 배열을 파라미터로 보내고 길이는 5임
 }
