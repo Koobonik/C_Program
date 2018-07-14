139f4ec5d58891a2738fa7a0109802c9e0b8690d
Koobonik <kbi960130@naver.com>
2018-07-14 19:56:47 +0900

    주석 추가

--- Call_By_Ref_Swap.c
+++ Call_By_Ref_Swap.c
@@ -8,12 +8,12 @@ void Swap(int * ptr1, int * ptr2)
 }
 void main()
 {
-    int num1 = 10;
-    int num2 = 20;
-    printf("num1 num2 : %d %d\n", num1, num2);
+    int num1 = 10; // 변수 선언
+    int num2 = 20; // 변수 선언
+    printf("num1 num2 : %d %d\n", num1, num2); // 변수 출력
     Swap(&num1, &num2);// 이 부분에서 서로 값을 바꿈
-    printf("num1 num2 : %d %d\n", num1, num2);// 값이 서로 바뀜
+    printf("num1 num2 : %d %d\n", num1, num2);// 값이 서로 바뀌어서 출력이 된다!
 
     Swap(&num1, &num2); // 다시 바꾸어줌
-    printf("num1 num2 : %d %d\n", num1, num2);
+    printf("num1 num2 : %d %d\n", num1, num2); // 다시 출력! 처음 그대로 상태일 것임
 }
