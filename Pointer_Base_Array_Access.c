0717c5f84f3641e731e4c2127d2f7414cf515cb5
Koobonik <kbi960130@naver.com>
2018-07-14 18:43:02 +0000

    주석 추가

--- Pointer_Base_Array_Access.c
+++ Pointer_Base_Array_Access.c
@@ -3,7 +3,7 @@ void main()
 {
     int arr[3] = {11, 22, 33};
     int *ptr = arr; // int *ptr = &arr[0] 과 같은 문장!!
-    printf("%d %d %d \n", *ptr, *(ptr+1), *(ptr+2));
+    printf("%d %d %d \n", *ptr, *(ptr+1), *(ptr+2)); // +1 이 아닌 다음 배열값을 불러오는것임
 
     printf("%d ", *ptr); ptr++; // 출력후 ptr++
     printf("%d ", *ptr); ptr++;
