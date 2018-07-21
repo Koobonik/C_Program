658541cf8898bd937b5a45c8760f7e169df86bf3
Koobonik <kbi960130@naver.com>
2018-07-21 11:11:39 +0000

    주석 추가

--- Use_value_reference.c
+++ Use_value_reference.c
@@ -2,21 +2,19 @@
 
 int SquareByValue(int a)
 {
-    int s = a * a; // 제곱 반환
-    return s;
+    return a*a; // 제곱 반환
 }
 
 void SquareByReference(int * b) // 인스턴스 자체에 다시 변수 대입
 {
-    int ss = * b;
+    int ss = * b; // * b 가 가리키는 것은 &num의 주소값
     *b = ss * ss;
 }
 
 void main()
 {
     int num = 30;
-    int * para = &num;
-
+    int * para = &num; // num의 주소값을 para 에 대입한다
     printf("%d \n", SquareByValue(num));
     SquareByReference(&num);
     printf("%d \n", num);
