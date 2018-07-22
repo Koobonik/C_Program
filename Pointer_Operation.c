9fe59be2f7a6e7575852558b67305163e4282898
Koobonik <kbi960130@naver.com>
2018-07-22 12:05:33 +0000

    주석 추가

--- Pointer_Operation.c
+++ Pointer_Operation.c
@@ -6,6 +6,6 @@ int main(void){
 	(*pnum) += 30; // num1 에는 130이 들어간다.
 	pnum = &num2; // pnum이 가리키는 것을 num2로 바꾼다
 	(*pnum) -= 50; // 50이 들어갈 것임
-	printf("num1 : %d , num2 : %d \n", num1, num2);
+	printf("num1 : %d , num2 : %d \n", num1, num2); // 130, 50이 출력될 것임
 	return 0;
 }
