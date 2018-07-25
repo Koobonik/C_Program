3991196dc15ae9fd144f5b6bb00237f4587458f1
Koobonik <kbi960130@naver.com>
2018-07-25 10:57:22 +0000

    문자열 두개를 입력 받아야 하므로 두개 입력하는 소스 코드 작성

--- Common_Prefix.c
+++ Common_Prefix.c
@@ -1,8 +1,12 @@
-/* 문자열 배열이 주어지면 제일 긴 공통된 접두사의 길이를 구하시오.*/
+/* 문자열 배열 두개가 주어지면 제일 긴 공통된 접두사의 길이를 구하시오.*/
 
 #include <stdio.h>
 void main(){
-    char input_string[50];
-    printf("문자를 입력해주세요 : \n");
-    scanf("%s", &input_string);
+    char one_input_string[50];
+    char two_input_string[50];
+    printf("첫번째 문자를 입력해주세요 : \n");
+    scanf("%s", one_input_string); // 문자열 입력
+
+    printf("두번째 문자를 입력해주세요 : \n");
+    scanf("%s",two_input_string); // 문자열 입력
 }
