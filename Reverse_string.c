694f370068f8d37986bf5a6465e3be4b86be2f9b
Koobonik <kbi960130@naver.com>
2018-07-29 12:39:22 +0000

    주석 추가

--- Reverse_string.c
+++ Reverse_string.c
@@ -8,17 +8,16 @@
 void main(){
     char string[50]; // 입력받은 문자열을 저장할 변수
     char reverse_string[50]; // 거꾸로 입력되는 문자열을 저장할 변수
-    int string_length;
-    int count = 0;
-    printf("문자열을 입력해주세요 : ");
-    scanf("%[^\n]s", string);
-    string_length = strlen(string);
+    int string_length; // 문자열의 길이를 저장할 변수
+    int count = 0; // 이후 reverse_string 에 저장될 문자들을 저장하기 위해 +1 씩 증가될 변수
+    printf("문자열을 입력해주세요 : "); // 문자열 입력
+    scanf("%[^\n]s", string); // [^\n] 의 기능은 \n을 받기 전까지 계속입력한다는 뜻이다.
+    string_length = strlen(string); // 길이 넣기
 
-    for(int i = string_length - 1; i >= 0; i--){
+    // 배열을 뒤집어줄 변수
+    for(int i = string_length - 1; i >= 0; i--){ //
         reverse_string[count] = string[i];
         count++;
     }
     printf("%s\n",reverse_string);
-
-
 }
