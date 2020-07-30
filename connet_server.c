835b4c8fa1aede646130505b248954f939e7ecba
Koobonik <kbi960130@naver.com>
2020-07-31 00:02:17 +0900

    .

--- connet_server.c
+++ connet_server.c
@@ -5,5 +5,4 @@ void main(int args, char **argv)
     printf("connet linux server\n");
     //int system(const char *string);
     int ret = system("sudo apt-get update");//입력하고 싶은 명령어 입력하면 됨
-    // 이후 ssh 연결을 편리하게 하기위해 만들 것임
 }
