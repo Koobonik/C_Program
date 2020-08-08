e23258e8f38bdc0fa1087159bcfe4a1f7812b512
Koobonik <kbi960130@naver.com>
2020-08-09 00:03:37 +0900

    삭제

--- connet_server.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-void main(int args, char **argv)
-{
-    printf("connet linux server\n");
-    int ret = system("sudo apt-get update");//입력하고 싶은 명령어 입력하면 됨
-}
