6132a7b5ffabada092de931f3cd08cf277bbc7b5
Koobonik <kbi960130@naver.com>
2020-08-14 00:04:15 +0900

    필요없는 라인 제거

--- Palindrome.c
+++ Palindrome.c
@@ -2,7 +2,6 @@
  회문을 작성할 것임
  앞으로 읽으나 뒤로 읽으나 차이가 없는 단어!
  */
-
 #include <stdio.h>
 #include <string.h> // 문자열 함수 사용
 
@@ -15,7 +14,6 @@ void main(){
     printf("입력한 문자열은 %s 입니다\n", c);
     int char_num = strlen(c); // 단어의 길이 계산
     printf("단어의 길이는 %d 입니다.\n", char_num);
-
     // 문자열을 반대로 저장할 것임
     for(int i = char_num - 1; i >= 0; i--){ // 배열값은 하나 더 작으므로
         d[e] = c[i]; // e == 0 이었으며 i == 단어의 길이
