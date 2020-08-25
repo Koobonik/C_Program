a431569a4468beda01d2cc1471d953dd2c4ee49f
Koobonik <kbi960130@naver.com>
2020-08-26 00:11:04 +0900

    매일매일 정말 힘들다

--- Convert_10_to_2.c
+++ Convert_10_to_2.c
@@ -3,7 +3,6 @@ void main(){
     int input; // 입력할 숫자
     int arr[30]; // 배열의 크기
     int count = 0; // 반복문을 몇번 돌려줄지 정해줄 변수
-    
     // 정수 입력 부분
     printf("정수를 입력하세요 : ");
     scanf("%d", &input);
@@ -14,7 +13,6 @@ void main(){
         input = input/2; // 그리고나서 2로 나누어준 값을 다시 대입
         count++; // 이후 배열을 몇번 반복할지 결정해줄 변수
     }
-
     // 배열에 담긴값을 출력
     for(int i = count - 1; i >= 0; i--){
         printf("%d", arr[i]);
